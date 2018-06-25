1e4be8814077a388ccd381f7c89cd7132b5a34dc
bharathparsam <bharathparsam@live.com>
2018-06-25 10:55:10 +0530

    Few Fixes

---
+++ src/SideNav.js
@@ -0,0 +1,54 @@
+import React, {
+  Component
+} from 'react';
+
+class SideNav extends Component {
+  state = {
+    query: ''
+  }
+
+
+  render = () => {
+    return ( <
+      div id = "nav" >
+      <
+      input type = "text"
+      placeholder = "Filter by location names"
+      id = "filter"
+      aria - label = "Search Filter"
+      onChange = {
+        this.props.filter
+      }
+      /> <
+      ul id = "items-list"
+      aria - label = "list of locations"
+      role = "navigate" > {
+        this.props.places.map((mark, index) => {
+            return ( < li key = {
+                index
+              }
+              className = "elementss"
+              onClick = {
+                this.props.openInfoWindow.bind(this, mark)
+              }
+              value = {
+                this.state.query
+              }
+              tabIndex = {
+                this.props.isOpen ? -1 : 0
+              } > {
+                mark.title
+              } < /li>)
+
+            })
+
+        }
+
+        <
+        /ul> <
+        /div>
+      );
+
+    }
+  }
+  export default SideNav
